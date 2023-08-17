2704a130a4ef76766081ff89db5d664bce7d8cde
arefks <arefks@gmail.com>
2023-08-17 15:52:33 +0200

    [DATALAD] Recorded changes

--- code/CSVplot.py
+++ code/CSVplot.py
@@ -9,7 +9,7 @@ from scipy.stats import ttest_ind
 from statsmodels.stats.multitest import multipletests
 import re
 
-#%% Function to read CSV files and store them in a dictionary
+#% Function to read CSV files and store them in a dictionary
 
 
 def read_csv_files(files):
@@ -18,14 +18,14 @@ def read_csv_files(files):
         df = pd.read_csv(file)    
         # Extract the data type from the file name (assuming the file name contains "anatomical", "structural", or "functional")
         data_type = "anatomical" if "anatomical" in file else ("structural" if "structural" in file else "functional")
-        data_dict[ff] = {data_type:df}
+        data_dict[ff] = {data_type:df} 
             
     return data_dict
 
 
 # Function for statistical comparison and plotting
 def compare_and_plot(data, column_name, group_column):
-    sns.set_palette("colorblind")  # Use colors for color-blind people
+    sns.set_palette("Set2")  # Use colors for color-blind people
     plt.figure(figsize=(10, 6))
     sns.boxplot(x=group_column, y=column_name, data=data)
     plt.xlabel(group_column)
@@ -36,7 +36,7 @@ def compare_and_plot(data, column_name, group_column):
     plt.show()
 
 #% List of CSV files for each data type
-Path = r"\\10.209.5.114\Publications\2023_Kalantari_AIDAqc\outputs\QC_new_studies_04\validation"
+Path = r"C:\Users\aswen\Desktop\validation"
 
 anatomical_files = glob.glob(os.path.join(Path,"**/*caculated_features_anatomical.csv"), recursive=True)
 structural_files = glob.glob(os.path.join(Path,"**/*caculated_features_structural.csv"), recursive=True)
@@ -80,6 +80,9 @@ for dd,data in enumerate(All_Data):
             ax = sns.violinplot(x="Dataset", y=feature, data=Data_of_selected_feature, hue="Dataset", dodge=False,
                                 palette=palette,
                                 scale="width", inner=None)
+            patches = ax.patches
+            legend_colors = [patch.get_facecolor() for patch in patches[:]]
+
             xlim = ax.get_xlim()
             ylim = ax.get_ylim()
             for violin in ax.collections:
@@ -97,24 +100,36 @@ for dd,data in enumerate(All_Data):
             ax.set_ylim(ylim)
             ax.legend_.remove()
             ax
-            ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=40, weight='bold')
+# =============================================================================
+#             for label, color in zip(ax.get_xticklabels(), legend_colors):
+#                 label.set_color(color)
+# =============================================================================
             ax.set_xlabel('')
-            ax.set_title(All_type[dd])
-           
+            ax.set_title(All_type[dd],weight='bold')
+            y_label = ax.set_ylabel(ax.get_ylabel(), fontweight='bold')
+
             ax.xaxis.grid(True, linestyle='-', which='major', color='gray', linewidth=1)
             ax.xaxis.grid(True, linestyle='--', which='minor', color='gray', linewidth=0.5)
 
             ax.yaxis.grid(True, linestyle='-', which='major', color='gray', linewidth=1)
             ax.yaxis.grid(True, linestyle='--', which='minor', color='gray', linewidth=0.5)        
 
-            plt.savefig(os.path.join(Path,feature+"_"+All_type[dd]+".svg"), format='svg', bbox_inches='tight')
+            plt.savefig(os.path.join(Path,feature+"_"+All_type[dd]+".svg"), format='svg', bbox_inches='tight',transparent=False)
             plt.show()
 #%% Data statistics figure 6
 
 
 p_address = r"\\10.209.5.114\Publications\2023_Kalantari_AIDAqc\docs\AIDAqc_Testdaten.csv"
-p_save = r"\\10.209.5.114\Publications\2023_Kalantari_AIDAqc\outputs\QC_new_studies_04\validation"
+p_save = r"\\10.209.5.114\Publications\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\PieChart"
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
 
+# Set the color palette to 'Set2'
+#sns.set_palette("Set2")
+sns.set(font='Times New Roman', font_scale=0.7,palette= "Set2",)
 # Load the CSV data into a pandas DataFrame
 data = pd.read_csv(p_address)
 # Clean up the 'Scanner' column to extract the field strength (including decimals)
@@ -128,70 +143,46 @@ def extract_field_strength(scanner):
 
 data['Scanner'] = data['Scanner'].apply(extract_field_strength)
 
-# =============================================================================
-# # 1. Sample size distribution
-# plt.figure(figsize=(10, 6))
-# sns.histplot(data=data, x='Sample (n)', bins=30, kde=True)
-# plt.xlabel('Sample Size')
-# plt.ylabel('Frequency')
-# plt.title('Sample Size Distribution')
-# plt.show()
-# =============================================================================
-sns.set_palette("colorblind")  # Use colors for color-blind people
-sns.set(font='Times New Roman', font_scale=0.65,style=None,rc={"font.weight": "bold"})  # Set font to Times New Roman and font size to 9
-# 2. Species pie chart
+# Species pie chart
 species_counts = data['Species'].value_counts()
 plt.figure(figsize=(2.77, 2.77), dpi=300)
-plt.pie(species_counts, labels=species_counts.index, autopct='%1.1f%%', startangle=140,pctdistance=0.85)
+plt.pie(species_counts, labels=species_counts.index, autopct='%1.1f%%', startangle=140, pctdistance=0.85)
+plt.title('Species Distribution', weight='bold').set_fontsize('9')
 
-
-sns.set(font='Times New Roman', font_scale=0.9,style=None,rc={"font.weight": "bold"})  # Set font to Times New Roman and font size to 9
-
-plt.title('Species Distribution')
-
-plt.savefig(p_save +'Species Distribution' +".svg", format='svg', bbox_inches='tight')
+plt.savefig(p_save + 'Species Distribution' + ".svg", format='svg', bbox_inches='tight')
 plt.show()
-# 3. Field strength pie chart
+
+# Field strength pie chart
 scanner_counts = data['Scanner'].value_counts()
 plt.figure(figsize=(2.77, 2.77), dpi=300)
-sns.set(font='Times New Roman', font_scale=0.7,style=None,rc={"font.weight": "bold"})  # Set font to Times New Roman and font size to 9
-plt.pie(scanner_counts, labels=scanner_counts.index, autopct='%1.1f%%', startangle=140,pctdistance=0.8)
-sns.set(font='Times New Roman', font_scale=0.9,style=None,rc={"font.weight": "bold"})  # Set font to Times New Roman and font size to 9
+plt.pie(scanner_counts, labels=scanner_counts.index, autopct='%1.1f%%', startangle=140, pctdistance=0.8)
+plt.title('Field Strength Distribution (T)', weight='bold').set_fontsize('9')
 
-plt.title('Field Strength Distribution (T)')
-
-plt.savefig(p_save +'Field Strength Distribution' +".svg", format='svg', bbox_inches='tight')
+plt.savefig(p_save + 'Field Strength Distribution' + ".svg", format='svg', bbox_inches='tight')
 plt.show()
-# 4. Sequence type pie chart
-# Split the combined sequences by commas and create a new DataFrame
-sequences_data = data['Sequences'].str.split(', ', expand=True)
 
-# Reshape the sequences DataFrame to long format
+# Sequence type pie chart
+sequences_data = data['Sequences'].str.split(', ', expand=True)
 sequences_melted = sequences_data.melt(value_name='Sequence').dropna()['Sequence']
 
-# Create the sequence type pie chart
 plt.figure(figsize=(2.77, 2.77), dpi=300)
 sequence_counts = sequences_melted.value_counts()
-plt.pie(sequence_counts, labels=sequence_counts.index, autopct='%1.1f%%', startangle=140,pctdistance=0.7)
-plt.title('Sequence Type Distribution')
+plt.pie(sequence_counts, labels=sequence_counts.index, autopct='%1.1f%%', startangle=140, pctdistance=0.7)
+plt.title('Sequence Type Distribution', weight='bold').set_fontsize('9')
 
-plt.savefig(p_save +'Sequence Type Distribution'+".svg", format='svg', bbox_inches='tight')
+plt.savefig(p_save + 'Sequence Type Distribution' + ".svg", format='svg', bbox_inches='tight')
 plt.show()
-# 5. Data format pie chart
+
+# Data format pie chart
 format_counts = data['Data format'].value_counts()
 plt.figure(figsize=(2.77, 2.77), dpi=300)
 plt.pie(format_counts, labels=format_counts.index, autopct='%1.1f%%', startangle=140)
-plt.title('Data Format Distribution')
+plt.title('Data Format Distribution', weight='bold').set_fontsize('9')
 
-plt.savefig(p_save +"Data Format Distribution" +".svg", format='svg', bbox_inches='tight')
+plt.savefig(p_save + "Data Format Distribution" + ".svg", format='svg', bbox_inches='tight')
 plt.show()
 
-
-
-
-
-
-
+#%%
 
 
 
