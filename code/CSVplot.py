2f1261c7cd511b1c10609b2aaff784660cb7e3ca
arefks <arefks@gmail.com>
2023-08-24 17:52:28 +0200

    [DATALAD] Recorded changes

--- code/CSVplot.py
+++ code/CSVplot.py
@@ -32,7 +32,7 @@ def compare_and_plot(data, column_name, group_column):
     plt.ylabel(column_name)
     plt.title(f"Statistical Comparison of {column_name} by {group_column}")
     plt.tight_layout()
-    plt.savefig(f"{column_name}_by_{group_column}_boxplot.png")
+    #plt.savefig(f"{column_name}_by_{group_column}_boxplot.png")
     plt.show()
 
 #% List of CSV files for each data type
@@ -76,7 +76,7 @@ for dd,data in enumerate(All_Data):
             
             
         if not Data_of_selected_feature.empty:
-            Data_of_selected_feature['sort'] = Data_of_selected_feature['Dataset'].str.extract('(\d+)', expand=False).astype(int)
+            Data_of_selected_feature['sort'] = Data_of_selected_feature['Dataset'].str.extract('(\d+)', expand=True).astype(int)
             Data_of_selected_feature = Data_of_selected_feature.sort_values('sort')
             
             if feature == "SNR Normal":
@@ -146,7 +146,7 @@ for dd,data in enumerate(All_Data):
             ax.spines['left'].set_linewidth(0.5)  # Left border
             ax.tick_params(axis='both', which='both', width=0.5,color='gray',length=2)
             plt.xticks(ha='right')
-            plt.savefig(os.path.join(Path,feature+"_"+All_type[dd]+".svg"), format='svg', bbox_inches='tight',transparent=False)
+            #plt.savefig(os.path.join(Path,feature+"_"+All_type[dd]+".svg"), format='svg', bbox_inches='tight',transparent=False)
             plt.show()
 #%% Data statistics figure 6
 
@@ -159,7 +159,7 @@ import seaborn as sns
 import re
 
 # Set the color palette to 'Set2'
-sns.set(font='Times New Roman', font_scale=0.8, palette="Set2")
+sns.set(font='Times New Roman', font_scale=0.8, palette="pastel")
 
 # Load the CSV data into a pandas DataFrame
 data = pd.read_csv(p_address)
@@ -205,12 +205,13 @@ for ax in axes:
 
 # Adjust layout and save the figure
 #plt.tight_layout()
-plt.savefig(p_save + "All_Pie_Charts" + ".svg", format='svg', bbox_inches='tight')
+#plt.savefig(p_save + "All_Pie_Charts" + ".svg", format='svg', bbox_inches='tight')
 plt.show()
 
-#%% SNR Chang vs Standard plot
+#%% SNR Chang vs Standard plot for RT and Cryp
 
-p_address = r"Y:\Student_projects\14_Aref_Kalantari_2021\Projects\QC\calculated_features\caculated_features_anatomical.csv"
+p_address = r"Z:\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\94_m_As\calculated_features\caculated_features_anatomical.csv"
+p_address2= r"Z:\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\94c_m_As\calculated_features\caculated_features_anatomical.csv"
 #p_save = r"\\10.209.5.114\Publications\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\ChangVSStandard"
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -218,6 +219,7 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 cm = 1/2.54  # centimeters in inches
 # Set the color palette to 'Set2'
@@ -225,17 +227,37 @@ cm = 1/2.54  # centimeters in inches
 plt.figure(figsize=(18*cm/3,7.30*cm),dpi=300)
 # Load the CSV data into a pandas DataFrame
 data = pd.read_csv(p_address)
+data2 = pd.read_csv(p_address2)
+
+
+
 chang = data["SNR Chang"].to_frame().rename(columns={"SNR Chang": "SNR"})
 normal = data["SNR Normal"].to_frame().rename(columns={"SNR Normal": "SNR"})
+chang["type"] = "chang_RT"
+normal["type"] = "normal_RT"
 
 
-chang["type"] = "chang"
-normal["type"] = "normal"
+
+chang2 = data2["SNR Chang"].to_frame().rename(columns={"SNR Chang": "SNR"})
+normal2 = data2["SNR Normal"].to_frame().rename(columns={"SNR Normal": "SNR"})
+chang2["type"] = "chang_Cryo"
+normal2["type"] = "normal_Cryo"
+
+
+
 
 Data_merged = data[["SNR Chang","SNR Normal"]]
 Data_merged.rename(columns={"SNR Normal":"SNR-standard"})
+Data_merged[["SNR Chang","SNR Normal"]] = Data_merged[["SNR Chang","SNR Normal"]].apply(lambda x: np.power((x/20),10))
 
-SNR = pd.concat([chang, normal])
+Data_merged2 = data2[["SNR Chang","SNR Normal"]]
+Data_merged2.rename(columns={"SNR Normal":"SNR-standard"})
+Data_merged2[["SNR Chang","SNR Normal"]] = Data_merged2[["SNR Chang","SNR Normal"]].apply(lambda x: np.power((x/20),10))
+
+
+SNR = pd.concat([chang, normal,chang2,normal2])
+SNR["SNR"] = SNR["SNR"].apply(lambda x: np.power(x/20,10))
+
 tips = SNR
 x = "type"
 y = "SNR"
@@ -248,20 +270,168 @@ circle_marker = {"marker": "o", "markerfacecolor": "black", "markersize": 3}
 plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['font.size'] = 8
 plt.title("(c) Chang vs Standard SNR",weight='bold')
-ax=sns.barplot(x="type", y="SNR", data=tips, capsize=0,palette="Set2",errorbar="se",ci=None)
+ax=sns.barplot(x="type", y="SNR", data=tips, capsize=0 ,palette="pastel",errorbar="se",ci=None)
 #sns.swarmplot(x="type", y="SNR", data=tips, color="0", alpha=.5)
 for i in range(len(Data_merged)):
-    plt.plot(["SNR Normal", "SNR-standard"], Data_merged.iloc[i], color="black", **circle_marker,linewidth=0.5)
+    plt.plot(["chang_RT", "normal_RT"], Data_merged.iloc[i], color="black", **circle_marker,linewidth=0.5)
+
+for i in range(len(Data_merged2)):
+    plt.plot(["chang_Cryo", "normal_Cryo"], Data_merged2.iloc[i], color="black", **circle_marker,linewidth=0.5)
 
 ax.spines['top'].set_linewidth(0)  # Top border
 ax.spines['right'].set_linewidth(0)  # Right border
 ax.spines['bottom'].set_linewidth(0.5)  # Bottom border
 ax.spines['left'].set_linewidth(0.5)  # Left border
 # Set the font to Times New Roman and font size to 8 points
-
+#ax.set_ylim(bottom=0, top=45)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=20,fontsize=8)
 # Rest of your code...
 ax.set_xlabel('')
 ax.tick_params(axis='both', which='both', width=0.5,color='gray',length=2)
 plt.show()
 
+
+#%% SNR Chang vs Standard plot for RT and Cryp Version 2
+
+p_address = r"Z:\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\94_m_As\calculated_features\caculated_features_anatomical.csv"
+p_address2= r"Z:\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\94c_m_As\calculated_features\caculated_features_anatomical.csv"
+#p_save = r"\\10.209.5.114\Publications\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\ChangVSStandard"
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+cm = 1/2.54  # centimeters in inches
+# Set the color palette to 'Set2'
+
+plt.figure(figsize=(18*cm/3,7.30*cm),dpi=300)
+# Load the CSV data into a pandas DataFrame
+data = pd.read_csv(p_address)
+data2 = pd.read_csv(p_address2)
+
+
+
+chang = data["SNR Chang"].to_frame().rename(columns={"SNR Chang": "SNR"})
+normal = data["SNR Normal"].to_frame().rename(columns={"SNR Normal": "SNR"})
+chang["type"] = "chang_rt"
+normal["type"] = "normal_rt"
+
+
+
+chang2 = data2["SNR Chang"].to_frame().rename(columns={"SNR Chang": "SNR"})
+normal2 = data2["SNR Normal"].to_frame().rename(columns={"SNR Normal": "SNR"})
+chang2["type"] = "chang_cryo"
+normal2["type"] = "normal_cryo"
+
+
+
+
+SNR = pd.concat([chang, normal,chang2,normal2])
+SNR["SNR"] = SNR["SNR"].apply(lambda x: np.power(10,x/20))
+
+
+SNR = SNR.sort_values('type')
+
+
+
+
+#Data_of_selected_feature = Data_of_selected_feature.sort_values("Dataset",ascending=False)
+# creating boxplots
+#plt.figure(figsize=(6*cm,6*cm*np.e),dpi=300)
+sns.set_style('ticks')
+sns.set(font='Times New Roman', font_scale=0.9,style=None)  # Set font to Times New Roman and font size to 9
+palette = 'Set2'
+ax = sns.violinplot(x="type", y="SNR", data=SNR, hue="type", dodge=False,
+                    palette=palette,
+                    scale="width", inner=None,linewidth=1)
+patches = ax.patches
+#legend_colors = [patch.get_facecolor() for patch in patches[:]]
+
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+for violin in ax.collections:
+    bbox = violin.get_paths()[0].get_extents()
+    x0, y0, width, height = bbox.bounds
+    violin.set_clip_path(plt.Rectangle((x0, y0), width / 2, height, transform=ax.transData))
+
+sns.boxplot(x="type", y="SNR", data=SNR, saturation=1, showfliers=False,
+            width=0.3, boxprops={'zorder': 3, 'facecolor': 'none'}, ax=ax, linewidth=1)
+old_len_collections = len(ax.collections)
+sns.stripplot(x="type", y="SNR", data=SNR, size=1.1, hue="type", palette=palette, dodge=False, ax=ax)
+for dots in ax.collections[old_len_collections:]:
+    dots.set_offsets(dots.get_offsets() + np.array([0.12, 0]))
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
+ax.legend_.remove()
+ax
+ax.set_xlabel('')
+ax.set_xticklabels(ax.get_xticklabels(), rotation=20,fontsize=8)
+#ax.set_ylim(bottom=15, top=40)
+
+#%% Plot all chang vs normal
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+import numpy as np
+
+
+cm = 1/2.54  # centimeters in inches
+# Specify the path to your Excel file
+excel_file_path = r"Z:\2023_Kalantari_AIDAqc\outputs\QC_Final\validation\Chang&normal_all.xlsx"
+plt.figure(figsize=(8*cm,8*cm),dpi=300)
+# Read the data into a pandas DataFrame
+df = pd.read_excel(excel_file_path, engine='openpyxl')
+
+# Drop rows with NaN values in the specified columns
+#df = df.dropna(subset=['SNR-Chang (dB)', 'SNR-Standard (dB)'])
+
+df['SNR-Chang'] = df['SNR-Chang'].apply(lambda x: np.power(10,x/20))
+df['SNR-Standard'] = df['SNR-Standard'].apply(lambda x: np.power(10,x/20))
+
+df = df.sort_values('sort')
+
+
+filtered_df = df[df['Dataset'].isin(['94_m_We', '94_m_Va'])]
+
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['font.size'] = 8
+plt.title("All anatomical data",weight='bold')
+
+# Calculate the correlation and p-value between 'SNR-Chang' and 'SNR-Standard'
+#correlation, p_value = stats.pearsonr(df['SNR-Chang (dB)'], df['SNR-Standard (dB)'])
+correlation, p_value = stats.spearmanr(df['SNR-Chang'], df['SNR-Standard'], nan_policy='omit',alternative='two-sided')
+
+# Set seaborn style
+sns.set_style('whitegrid')
+
+# Create a scatter plot
+#ax=sns.scatterplot(data=df, x='SNR-Chang', y='SNR-Standard',palette="gray_r",s=7)
+ax=sns.scatterplot(data=df, x='SNR-Chang', y='SNR-Standard',hue="Dataset",palette="Spectral",s=7)
+
+
+
+
+# Set title and labels including the correlation and p-value
+#plt.title(f'Correlation: {correlation:.8f}, P-value: {p_value:.8f}')
+plt.xlabel('SNR-Chang')
+plt.ylabel('SNR-Standard')
+
+ax.set_xlim(0,200)
+ax.spines['top'].set_linewidth(0)  # Top border
+ax.spines['right'].set_linewidth(0)  # Right border
+ax.spines['bottom'].set_linewidth(0.5)  # Bottom border
+ax.spines['left'].set_linewidth(0.5)  # Left border
+# Show the plot
+# Move the legend outside the plot to the right side
+legend =plt.legend(title="Dataset", loc='center left', bbox_to_anchor=(1, 0.5), fontsize=5,handlelength=0.5)
+legend.get_title().set_fontfamily('Times New Roman')
+for text in legend.get_texts():
+    text.set_fontfamily('Times New Roman')
+
+plt.show()
 
